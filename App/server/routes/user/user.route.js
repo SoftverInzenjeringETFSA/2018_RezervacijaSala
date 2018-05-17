24e54bb0af3f0ad2ed200dfd54f7cc1f2309a205
Mustafa Mahmutovic <mustafa.mahmutovic93@gmail.com>
2018-05-17 12:10:09 +0200

    Typo fix

--- App/server/routes/user/user.route.js
+++ App/server/routes/user/user.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const UserRouter = express.Router();
-const UserController = require('./User.controller');
+const UserController = require('./user.controller');
 
 UserRouter.post('/create', UserController.POST_Create);
 UserRouter.post('/login', UserController.LOGIN);
