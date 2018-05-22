5c579df1419c7524ff6aee3747a7dad1839de324
Nedim Muratović <nmuratovic1@etf.unsa.ba>
2018-05-22 14:48:36 +0200

    Resolved conflicts.

--- App/server/routes/index.route.js
+++ App/server/routes/index.route.js
@@ -3,11 +3,8 @@ const express = require('express');
 const IndexRouter = express.Router();
 const AuthRouter = require('./auth/auth.route');
 const ScheduleRouter = require('./schedule/schedule.route');
-<<<<<<< HEAD
 const ReservationRouter = require('./reservation/reservation.route');
-=======
 const SemesterRouter = require('./semester/semester.route');
->>>>>>> master
 
 // TODO: Remove after frontend development starts.
 const TestRouter = require('./test/test.route');
@@ -15,10 +12,7 @@ IndexRouter.use('/test', TestRouter);
 
 IndexRouter.use('/auth', AuthRouter);
 IndexRouter.use('/schedule', ScheduleRouter);
-<<<<<<< HEAD
 IndexRouter.use('/reservation', ReservationRouter);
-=======
 IndexRouter.use('/semester', SemesterRouter);
->>>>>>> master
 
 module.exports = IndexRouter;
