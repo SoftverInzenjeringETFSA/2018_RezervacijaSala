71182fda8b41bfe8ec87e05fe0221fa11bbf7225
Mustajbasic <belmin.mustajbasic@gmail.com>
2018-05-22 11:44:05 +0200

    Added: React router and bootstrap menu. Also added some files for schedule

---
+++ App/client/src/containers/home/home.container.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomeContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>Hello from home</h1>
+    }
+}
+
+export default HomeContainer;
